29182602955789badc0c7240812186feefe8af9c
sergio-malysh <sergio_malysh@mail.ru>
2016-01-05 16:46:43 +0200

    spring_cache

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package ru.javawebinar.topjava.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.ExceptionUtil;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.Collection;
+import java.util.Objects;
 import java.util.List;
 
 /**
@@ -16,13 +21,16 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
+    @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
     private UserRepository repository;
 
+    @CacheEvict(value = "users", allEntries = true)
     public User save(User user) {
         return repository.save(user);
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void delete(int id) {
         ExceptionUtil.check(repository.delete(id), id);
     }
@@ -32,14 +40,22 @@ public class UserServiceImpl implements UserService {
     }
 
     public User getByEmail(String email) throws NotFoundException {
+        Objects.requireNonNull(email, "Email must not be empty");
         return ExceptionUtil.check(repository.getByEmail(email), "email=" + email);
     }
 
+    @Cacheable("users")
     public List<User> getAll() {
         return repository.getAll();
     }
 
+    @CacheEvict(value = "users", allEntries = true)
     public void update(User user) {
         repository.save(user);
+    }
+
+    @CacheEvict(value = "users", allEntries = true)
+    @Override
+    public void evictCache() {
     }
 }
